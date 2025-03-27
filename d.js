ece4623cd3122da42fbc3a48ab9fb48e1c3e3823
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-03-27 16:30:23 +0600

    Add files via upload

---
+++ d.js
@@ -0,0 +1,8 @@
+// Function to handle form submission and fetch data
+async function get() {
+   window.open("file:///C:/Users/admin/OneDrive/Desktop/diu%20updated/index.html")
+    }
+    async function get1() {
+        window.open("file:///C:/Users/admin/Videos/Bandicam/website/website/src/main/resources/static/index.html")
+         }
+     
