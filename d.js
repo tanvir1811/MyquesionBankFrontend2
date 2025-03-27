d336ea9e30881ceed24f7e0bb060412cde7ce03e
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-03-27 16:35:10 +0600

    Update d.js

--- d.js
+++ d.js
@@ -3,6 +3,6 @@ async function get() {
    window.open("file:///C:/Users/admin/OneDrive/Desktop/diu%20updated/index.html")
     }
     async function get1() {
-        window.open("file:///C:/Users/admin/Videos/Bandicam/website/website/src/main/resources/static/index.html")
+        window.open("https://tanvir1811.github.io/myquestionbankfrontend/")
          }
      
