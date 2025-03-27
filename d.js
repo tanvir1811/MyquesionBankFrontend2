3b6153eb95e704c18046a4f3c7380c81ba0cfddf
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-03-27 16:37:30 +0600

    Update d.js

--- d.js
+++ d.js
@@ -1,6 +1,6 @@
 // Function to handle form submission and fetch data
 async function get() {
-   window.open("file:///C:/Users/admin/OneDrive/Desktop/diu%20updated/index.html")
+   window.open("")
     }
     async function get1() {
         window.open("https://tanvir1811.github.io/myquestionbankfrontend/")
