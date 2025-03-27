9f80c110f9c489f4eea537bcca47b73054e7c8b1
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-03-27 19:46:01 +0600

    Update d.js

--- d.js
+++ d.js
@@ -1,6 +1,6 @@
 // Function to handle form submission and fetch data
 async function get() {
-   window.open("")
+   window.open("https://myspringboot-10.onrender.com/")
     }
     async function get1() {
         window.open("https://tanvir1811.github.io/myquestionbankfrontend/")
